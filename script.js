52053c81b3df92e72cedba2344feb5faf8f3c571
Abderrahmane Rahmani <83463523+rahmaniae@users.noreply.github.com>
2023-09-08 15:19:02 -0700

    Add function to play a round
    Create a function that work for one round to determine the winner and increment their score

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function getComputerSelection() {
     computerSelection = (choice == 0) ? "rock" : (choice == 1) ? "paper" : "scissor";
     console.log("computer : " + computerSelection);
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection == "rock" && computerSelection == "paper" 
+        || playerSelection == "paper" && computerSelection == "scissor" 
+        || playerSelection == "scissor" && computerSelection == "rock") {
+        console.log("You lost the round");
+        computerScore++;
+    }else if(playerSelection == computerSelection) {
+        console.log("Draw");
+    }else {
+        console.log("You win the round");
+        playerScore++;
+    }
+}
