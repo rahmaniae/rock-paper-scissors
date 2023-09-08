8bf24ed74200ffca45164eb50be5654add0e1d71
Abderrahmane Rahmani <83463523+rahmaniae@users.noreply.github.com>
2023-09-08 15:13:42 -0700

    Create a function to determine computer selection
    It determines computer selection randomly, then store it in 'computerSelection' variable declared previously

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
 
+function getComputerSelection() {
+    const choice = Math.floor(Math.random() * 3);
+    computerSelection = (choice == 0) ? "rock" : (choice == 1) ? "paper" : "scissor";
+    console.log("computer : " + computerSelection);
+}
